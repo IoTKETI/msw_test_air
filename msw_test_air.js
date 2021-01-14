e6ab2726b2f772f65613b1a2cf8dc9976f662932
IoTKETI <IoTKETI@github.com>
2021-01-14 15:52:42 +0900

    commit by KETI

--- msw_test_air.js
+++ msw_test_air.js
@@ -315,9 +315,6 @@ function parseDataMission(topic, str_message) {
            var topic_arr = topic.split('/');
          var data_topic = '/Mobius/' + config.gcs + '/Mission_Data/' + config.drone + '/' + config.name + '/' + topic_arr[topic_arr.length-1];
          msw_mqtt_client.publish(data_topic + '/' + my_sortie_name, str_message);
-  var _topic = '/MUV/control/' + config.lib[0].name + '/' + config.lib[0].control[0];
- var req = '1';
-  msw_mqtt_client.publish(_topic, req);
   }
   catch (e) {
     console.log('[parseDataMission] data format of lib is not json');
